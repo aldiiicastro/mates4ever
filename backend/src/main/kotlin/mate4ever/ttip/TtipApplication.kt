76cddd6aac043b41b2e3e1251899bf6d94f45ae3
Aldana Castro <aldana.castro@alu.unq.edu.ar>
2022-08-30 13:08:50 -0300

    First Commit

---
+++ backend/src/main/kotlin/mate4ever/ttip/TtipApplication.kt
@@ -0,0 +1,11 @@
+package mate4ever.ttip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TtipApplication
+
+fun main(args: Array<String>) {
+	runApplication<TtipApplication>(*args)
+}
---
+++ backend/src/test/kotlin/mate4ever/ttip/TtipApplicationTests.kt
@@ -0,0 +1,13 @@
+package mate4ever.ttip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TtipApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
